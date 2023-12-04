b08ae4fc91cdda2ead2c0e95a455c9d890ee224f
Rohit-Mahato06 <146702013+Rohit-Mahato06@users.noreply.github.com>
2023-12-04 20:33:13 +0545

    Delete Projects/news_system/admin directory

--- Projects/news_system/admin/db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$server = "localhost";
-$database = "news_system";
-$username = "root";
-$password = "";
-$con = new mysqli($server, $username, $password, $database);
-if (!$con) {
-    echo "db not connect";
-}
